79c33175df5df26908931371d34385c12b266d6d
TaranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2022-11-30 07:02:25 +0800

    delete all

--- data/headerNavLinks.ts
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/links', title: 'Links' },
-  { href: '/about', title: 'About' },
-  { href: 'https://github.com/users/taranakineko/projects/1/views/1', title: 'ToDo' },
-]
-
-export default headerNavLinks

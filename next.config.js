becb0d974ebe4b3fd93642940282bf287c0c94e7
TaranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2022-11-29 19:10:44 +0800

    放行 umami 相关

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self' fonts.googleapis.com;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app umami.nekoq.eu.org;
   style-src 'self' 'unsafe-inline' fonts.googleapis.com;
   img-src * blob: data:;
   media-src 'none';

7d72609dc251b0057df1dc5d62cf3d2e5044a2d0
TaranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2022-12-10 02:30:44 +0800

    添加链接 书签

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/links', title: 'Links' },
   { href: '/about', title: 'About' },
   { href: 'https://github.com/users/taranakineko/projects/1/views/1', title: 'ToDo' },
+  { href: 'https://taranakineko.notion.site/4fb811a3549746cf956571ad713426ad', title: 'Bookmark' },
 ]
 
 export default headerNavLinks

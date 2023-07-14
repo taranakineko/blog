5f90c8c37cf39d2dedb86f3b3d8ac3d412afce41
TaranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2023-07-14 20:59:57 +0800

    test

---
+++ themes/stellaris/layout/components/plugins/comments/utterances/script.jsx
@@ -0,0 +1,34 @@
+const UtterancesScript = props => {
+    const generatedScript = `
+      function loadUtterances() {
+        const els = document.querySelectorAll("#comments #utterances");
+        if (els.length === 0) return;
+        els.forEach((el, i) => {
+          try {
+            el.innerHTML = '';
+          } catch (error) {
+            console.log(error);
+          }
+          var script = document.createElement('script');
+          script.src = 'https://utteranc.es/client.js';
+          script.async = true;
+          for (let key of Object.keys(el.attributes)) {
+            let attr = el.attributes[key];
+            if (['class', 'id'].includes(attr.name) === false) {
+              script.setAttribute(attr.name, attr.value);
+            }
+          }
+          el.appendChild(script);
+        });
+      }
+      InstantClick.on('change', () => {
+        loadUtterances();
+      });
+      window.addEventListener(
+          "load", loadUtterances, false
+      );
+    `
+    return <script data-no-instant="true" dangerouslySetInnerHTML={{__html: generatedScript}}/>
+}
+
+module.exports = UtterancesScript;
---
+++ themes/stellaris/layout/index.jsx
@@ -0,0 +1,136 @@
+const {Fragment} = require('react');
+const NavBarListPost = require('./components/main/navbar/list_post.jsx');
+const NavBarListWiki = require('./components/main/navbar/list_wiki.jsx');
+const PostCard = require('./components/main/post_list/post_card.jsx');
+const WikiCard = require('./components/main/post_list/wiki_card.jsx');
+const Paginator = require('./components/main/post_list/paginator.jsx');
+
+const LayoutPostCard = (props) => {
+    const {post, url_for, scroll_reveal} = props;
+    let className;
+    if (post.cover !== undefined && post.poster !== undefined) {
+        className = 'post-card post photo ';
+    } else {
+        className = 'post-card post ';
+    }
+    className += scroll_reveal();
+    return (
+        <a className={className} href={url_for(post.link || post.path)} key={post.path}>
+            <PostCard post={post} {...props}/>
+        </a>
+    )
+}
+const LayoutWikiCard = (props) => {
+    const {proj, url_for, scroll_reveal} = props;
+    let className;
+    if (proj.cover !== undefined && proj.poster !== undefined) {
+        className = 'post-card wiki photo ';
+    } else {
+        className = 'post-card wiki ';
+    }
+    className += scroll_reveal();
+    return (
+        <a className={className} href={url_for(proj.homepage.link || proj.homepage.path)} key={proj.path}>
+            <WikiCard proj={proj} {...props}/>
+        </a>
+    )
+}
+
+const LayoutPosts = props => {
+    const PostCards = props => {
+        const {config, is_home, page, site} = props;
+        const post_cards = [];
+        const posts = page.posts || props.posts;
+        if (is_home()) {
+            if (page.current === 1) {
+                // Pinned posts
+                const pinned = site.posts.filter((post) => post.pin !== undefined).sort(
+                    (config.index_generator && config.index_generator.order_by) || '-date'
+                );
+                pinned.forEach(post => {
+                    post_cards.push(<LayoutPostCard post={post} {...props}/>);
+                })
+            }
+            // Normal posts
+            posts.forEach(post => {
+                if (post.pin === undefined) {
+                    post_cards.push(<LayoutPostCard post={post} {...props}/>);
+                }
+            })
+        } else {
+            posts.forEach(post => {
+                if (post.pin === undefined) {
+                    post_cards.push(<LayoutPostCard post={post} {...props}/>);
+                }
+            })
+        }
+
+        return post_cards;
+    }
+    return (
+        <div className="post-list post">
+            <PostCards {...props}/>
+        </div>
+    )
+}
+
+const LayoutWikis = (props) => {
+    const {page, theme} = props;
+    const elements = [];
+    const projects = theme.wiki.projects;
+    for (let proj_name of Object.keys(projects)) {
+        let proj = projects[proj_name];
+        if (proj.index === false || proj.pages === undefined || proj.pages.length === 0) {
+            continue;
+        }
+        if (page.filter === false) {
+            // all wikis
+            elements.push(
+                <div className="post-list wiki" key={proj_name}>
+                    <LayoutWikiCard proj={proj} {...props}/>
+                </div>
+            )
+        } else if (proj.tags && proj.tags.includes(page.tagName) === true) {
+            // filtered wikis
+            elements.push(
+                <div className="post-list wiki filter" key={proj_name}>
+                    <LayoutWikiCard proj={proj} {...props}/>
+                </div>
+            )
+        }
+    }
+    return elements;
+}
+
+module.exports = function Index(props) {
+    const {page} = props;
+
+    if (page.menu_id === undefined) {
+        if (page.layout === 'wiki_index' && page.wiki) {
+            page.menu_id = 'wiki';
+        } else {
+            page.menu_id = 'post';
+        }
+    }
+
+    if (page.title && page.wiki) {
+        page.robots = 'noindex,follow';
+    }
+
+    if (page.menu_id === 'post') {
+        return (
+            <Fragment>
+                <NavBarListPost {...props}/>
+                <LayoutPosts {...props}/>
+                <Paginator {...props}/>
+            </Fragment>
+        );
+    } else if (page.menu_id === 'wiki') {
+        return (
+            <Fragment>
+                <NavBarListWiki {...props}/>
+                <LayoutWikis {...props}/>
+            </Fragment>
+        );
+    }
+}

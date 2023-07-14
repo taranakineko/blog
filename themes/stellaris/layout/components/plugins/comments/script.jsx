6d26888ce073697005ab60e8d4ad64c2018ec1b0
TaranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2023-07-14 20:43:47 +0800

    Test

--- themes/stellaris/layout/components/plugins/comments/script.jsx
+++
@@ -1,9 +0,0 @@
-const CommentsScript = props => {
-    const {theme} = props;
-    const CommentScript = require('./' + theme.comments.service + '/script.jsx');
-    return (
-        <CommentScript {...props} />
-    )
-}
-
-module.exports = CommentsScript;

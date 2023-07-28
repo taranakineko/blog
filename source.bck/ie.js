5326951f94111c8d5442b7c43278510e09e66d53
TaranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2023-07-28 21:32:54 +0800

    del source.bak

--- source.bck/ie.js
+++
@@ -1,5 +0,0 @@
-if (navigator.userAgent.indexOf('compatible') > -1 && navigator.userAgent.indexOf('MSIE') > -1) {
-  window.location.replace('/ie.html')
-} else if (!!window.ActiveXObject || 'ActiveXObject' in window) {
-  window.location.replace('/ie.html')
-}
--- source.bck/others/index.md
+++
@@ -1,9 +0,0 @@
----
-title: 其他
-date: 2022-12-27 14:42:28
-summary: 这里包含了其他的东西（点头）
----
-
-[ToDo List](https://github.com/users/taranakineko/projects/1)
-
-[Bookmarks](https://bookmarks.nekoq.cyou/)

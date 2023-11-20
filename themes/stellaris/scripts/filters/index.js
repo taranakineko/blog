d24e98f6cf269fc5f32cac1e7f500533ccca1da6
taranakineko <55310629+taranakineko@users.noreply.github.com>
2023-11-20 23:14:12 +0800

    del modules

--- themes/stellaris/scripts/filters/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-hexo.extend.filter.register('after_render:html', require('./img_lazyload').processSite);
-hexo.extend.filter.register('after_render:html', require('./img_onerror').processSite);
-hexo.extend.filter.register('before_post_render', require('./change_image').changeImage, 9);
